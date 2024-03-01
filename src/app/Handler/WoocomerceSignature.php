e0138e2d79e212815d7a3ae8e06b60e0b7fe1d47
juangh27 <101841901+juangh27@users.noreply.github.com>
2024-03-01 16:36:48 -0600

    signature for webhook added

---
+++ src/app/Handler/WoocomerceSignature.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Handler;
+
+use Illuminate\Http\Request;
+use Spatie\WebhookClient\Exceptions\WebhookFailed;
+use Spatie\WebhookClient\WebhookConfig;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
+
+class WoocomerceSignature implements SignatureValidator
+{
+    public function isValid(Request $request, WebhookConfig $config): bool
+    {
+        $signature = $request->header($config->signatureHeaderName);
+        if (!$signature) {
+            return false;
+        }
+        $signingSecret = $config->signingSecret;
+
+
+        $computedSignature = base64_encode(hash_hmac('sha256', $request->getContent(), $signingSecret));
+        // $computedSignature = hash_hmac('sha256', $request->getContent(), $signingSecret);
+        return hash_equals($signature, $computedSignature);
+    }
+}
