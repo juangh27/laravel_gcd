f5ea700867a466e7ed8bdc4a714974f27cdabb95
juangh27 <101841901+juangh27@users.noreply.github.com>
2024-03-05 10:04:54 -0600

    testing webhook payload

--- src/app/Jobs/ProcessWebhookJob.php
+++ src/app/Jobs/ProcessWebhookJob.php
@@ -2,15 +2,11 @@
 
 namespace App\Jobs;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use App\Models\Registros;
 use App\Models\OrdenesCompra;
 use Illuminate\Support\Arr;
-use Spatie\WebhookClient\Models\WebhookCall;
+use Codexshaper\WooCommerce\Facades\Order;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob as SpatieProcessWebhookJob;
 
 class ProcessWebhookJob extends SpatieProcessWebhookJob implements ShouldQueue
@@ -25,12 +21,13 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob implements ShouldQueue
      */
     public function handle(): void
     {
-        $event = Arr::get($this->webhookCall->payload, 'arg');
+        $order_id = Arr::get($this->webhookCall->payload, 'arg');
         // $data = Arr::get($this->webhookCall->payload, 'data', []);
         // $data_test= json_decode($this->webhookCall, true);
         // $data_testing = $data_test['payload'];
         // $dat = json_decode($this->webhookCall, true);
         // $data = $dat['payload'];
+
 
         $register = new Registros;
         $register->user_id = 5; // Set user_id if applicable
@@ -42,10 +39,13 @@ class ProcessWebhookJob extends SpatieProcessWebhookJob implements ShouldQueue
         // $var = var_export($data_testing);
 
 
+
+
         $orden = new OrdenesCompra;
         // $orden->json = "$dat";
 
-        $orden->texto = $event;
+        $orden->texto = $order_id;
+        $orden->json = Order::find($order_id);
         $orden->save();
 
 
